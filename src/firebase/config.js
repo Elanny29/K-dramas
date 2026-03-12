1faa20fb6068f2e85c7da26ce0dee070e9a414fb
Elanny <ynnale@outlook.com>
2026-03-12 09:50:53 -0600

    cambio de credenciales

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,12 +6,10 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuYM7zKcT5oA4rTIodZPIKoKCI6c9ojOI",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "k-dramas-6b7fc.firebaseapp.com",
   projectId: "k-dramas-6b7fc",
-  storageBucket: "k-dramas-6b7fc.firebasestorage.app",
-  messagingSenderId: "113714343853",
-  appId: "1:113714343853:web:1c456f11465c25e5cb3b24"
+  storageBucket: "k-dramas-6b7fc.firebasestorage.app"
 };
 
 const app  = initializeApp(firebaseConfig);
